4ba75c81c6c56a4c74bd1c1ba0ca538a496cbf80
Adithya212 <81566547+Adithya212@users.noreply.github.com>
2023-02-09 04:30:23 +0530

    Update pass2.c

--- pass2.c
+++ pass2.c
@@ -1,63 +1,84 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void main()
- {
-char opcode[20],operand[20],symbol[20],label[20],code[20],mnemonic[25], character,
-add[20],objectcode[20];
-int flag,flag1,locctr,location,loc;
-FILE *fp1,*fp2,*fp3,*fp4;
-fp1=fopen("out1.txt","r"); fp2=fopen("twoout.txt","w");
-fp3=fopen("opcode.txt","r"); fp4=fopen("sym1.txt","r");
-fscanf(fp1,"%s%s%s",label,opcode,operand);
-if(strcmp(opcode,"START")==0)
-{ fprintf(fp2,"%s\t%s\t%s\n",label,opcode,operand);
-fscanf(fp1,"%d%s%s%s",&locctr,label,opcode,operand);
-}
-while(strcmp(opcode,"END")!=0)
-{ flag=0;
-fscanf(fp3,"%s%s",code,mnemonic);
-while(strcmp(code,"END")!=0)
-{ if((strcmp(opcode,code)==0) && (strcmp(mnemonic,"*"))!=0)
-{ flag=1;
-break;
-}
-fscanf(fp3,"%s%s",code,mnemonic);
-}
-if(flag==1)
-{ flag1=0; rewind(fp4);
-while(!feof(fp4))
+
+int main()
 {
-fscanf(fp4,"%s%d",symbol,&loc);
-if(strcmp(symbol,operand)==0)
-{
-flag1=1; break;
-} }
-if(flag1==1)
-{
-sprintf(add,"%d",loc);
-strcpy(objectcode,strcat(mnemonic,add));
-} }
-else if(strcmp(opcode,"BYTE")==0 || strcmp(opcode,"WORD")==0)
-{
-if((operand[0]=='C') || (operand[0]=='X'))
-{
-character=operand[2];
-sprintf(add,"%d",character);
-strcpy(objectcode,add);
-}
-else
-{
-strcpy(objectcode,add);
-} }
-else
-strcpy(objectcode,"\0");
-fprintf(fp2,"%s\t%s\t%s\t%d\t%s\n",label,opcode,operand,locctr,objectcode);
-fscanf(fp1,"%d%s%s%s",&locctr,label,opcode,operand);
-}
-fprintf(fp2,"%s\t%s\t%s\t%d\n",label,opcode,operand,locctr);
-fclose(fp1);
-fclose(fp2);
-fclose(fp3);
-fclose(fp4);
+	char label[10],opcode[10],operand[10],code[10],symtab[10],symadd[10],locctr[10],obj[10];
+	int length,i,start,flag=0;
+	FILE *fim,*foc,*fal,*fst,*fot,*flen;
+	fim = fopen("output.txt","r");
+	foc = fopen("objectcode.txt","w");
+	fal = fopen("assmlist.txt","w");
+
+	fscanf(fim,"\t%s\t%s\t%s",label,opcode,operand);
+	start=atoi(operand);   //strtil(operand,null,16)
+	flen = fopen("length.txt","r");
+	fscanf(flen,"%d",&length);
+	
+	if(strcmp(opcode,"START")==0)
+	{
+		fprintf(fal,"\t%s\t%s\t%s",label,opcode,operand);
+		fprintf(foc,"H^%6s^%6s^%6d\nT^%6s^%2d^",label,operand,length,operand,length);
+		fscanf(fim,"%s\t%s\t%s\t%s",locctr,label,opcode,operand);	
+		fprintf(fal,"\n%s\t%s\t%s\t%s\t",locctr,label,opcode,operand);
+	}
+	while(strcmp(opcode,"END")!=0)
+	{	
+		if(strcmp(opcode,"WORD")==0||strcmp(opcode,"BYTE")==0)
+		{
+			if(operand[0]=='C'||operand[0]=='X')
+			{
+				for(i=2;i<strlen(operand)-1;i++)
+				{
+						fprintf(foc,"%d",operand[i]);
+						fprintf(fal,"%d",operand[i]);
+				}
+			}
+				else
+				{
+					fprintf(foc,"%s^",operand);
+					fprintf(fal,"%s",operand);
+				}			
+		}
+		else
+		{
+			fot = fopen("optab.txt","r");
+			fscanf(fot,"%s\t%s",code,obj);
+			while(strcmp(code,"END")!=0)
+			{
+				if(strcmp(code,opcode)==0)
+				{    
+					fprintf(foc,"%s",obj); 
+					fprintf(fal,"%s",obj); 
+					fclose(fot);
+					fst = fopen("symtab.txt","r");
+					fscanf(fst,"%s\t%s",symtab,symadd);
+					while(strcmp(symtab,"END")!=0)
+					{
+						if(strcmp(operand,symtab)==0)
+						{   fclose(fst);
+							fprintf(foc,"%s^",symadd);
+							fprintf(fal,"%s",symadd);
+							flag=1;							
+							break;
+						}
+						fscanf(fst,"%s\t%s",symtab,symadd);
+					}
+				}
+				if(flag==1)
+					break;
+				fscanf(fot,"%s\t%s",code,obj);
+			}
+		}
+		fscanf(fim,"%s\t%s\t%s\t%s",locctr,label,opcode,operand);
+		fprintf(fal,"\n%s\t%s\t%s\t%s\t",locctr,label,opcode,operand);
+		flag=0;
+	}	
+	fprintf(foc,"\nE^%6d",start);
+	printf("\n\nObject code created in\"objectcode.txt\"\nAssembly list created in \"assmlist\"\n\n");
+	fclose(fim);
+	fclose(foc);
+	fclose(fal);
+	return 0;
 }
